1ef56359af6aaede2d11c591bee638349e252d0d
Aditya Pandhare <adityapandhare44@gmail.com>
2023-07-28 23:37:01 +0400

    Added Basic Player Movement
    I ensured there is encapsulation of variables and methods for each class.
    
    I also worked on player input and movement. For this this one I managed to work on WASD player movement. I also worked on the player camera and make it into an FPS style game.
    
    I also added a jumping mechanic if the user presses spacebar as well as a sprinting mechanic is the user presses Left-Shift

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float verticalInput;
-    public float speed = 10;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float playerWalkSpeed = 10;
+    [SerializeField] private float playerRunSpeed = 20;
+    private bool isRunning = false;
+
+    private bool isOnGround = true;
+    private Rigidbody playerRb;
+    [SerializeField] private float jumpForce = 300;
+
+    //Start is called before the first frame update
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // This gets the Horizontal Axis Movement from the Input Manager into a horizontalInput variable (1, -1).
-        horizontalInput = Input.GetAxis("Horizontal");
+        GetPlayerInput();
+    }
 
-        // This gets the Vertical Axis Movement from the Input Manager into a verticalInput variable (1, -1).
-        verticalInput = Input.GetAxis("Vertical");
+    //This is to ensure that the physics applied on the user aren't jittery
+    private void FixedUpdate() {
+        MovePlayer();
+    }
 
-        // If verticalInput is > 0, then it will move forward, else it will move backwards
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    //Monitors the playey's input
+    private void GetPlayerInput() {
+        //Gets movement input from WASD
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        // If horizontalInput is > 0, then it will move right, else it will move left
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        //Gets the jumping movement input from Spacebar
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            PlayerJump();
+            isOnGround = false;
+        }
+
+        //If the Left Shift Key Button is held down, then the player will be able to run. 
+        if(Input.GetKey(KeyCode.LeftShift)) {isRunning = true; }
+        else {isRunning = false; }
+    }
+
+    //Player moves forward/backwards and left/right depending on speed and input value. 
+    private void MovePlayer() {
+        //If Left shift isn't pressed, the player moves in walking speed
+        if(isRunning == true) {
+            transform.Translate(Vector3.forward * Time.deltaTime * playerRunSpeed * verticalInput);
+            transform.Translate(Vector3.right * Time.deltaTime * playerRunSpeed * horizontalInput);
+        }
+
+        //Else the player will move at running speed
+        else {
+            transform.Translate(Vector3.forward * Time.deltaTime * playerWalkSpeed * verticalInput);
+            transform.Translate(Vector3.right * Time.deltaTime * playerWalkSpeed * horizontalInput);
+        }
+    }
+
+    //This function resets the players y-velocity and adds an upwards force on them for a jump
+    private void PlayerJump() {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    //This simply checks all the collision objects that the player can collide with
+    private void OnCollisionEnter(Collision other) {
+        //If the other object is the ground, then it will check if the player is still on it. 
+        if(other.gameObject.CompareTag("Ground")) {
+            isOnGround = true;
+        }
     }
 }
